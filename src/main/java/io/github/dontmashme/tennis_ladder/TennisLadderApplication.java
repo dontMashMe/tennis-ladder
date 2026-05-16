f242940c45557f9aaa064740001507cdea60a978
Marijan Bebek <marijan.bebek@mail.com>
2026-05-16 16:35:20 +0200

    Init commit

---
+++ src/main/java/io/github/dontmashme/tennis_ladder/TennisLadderApplication.java
@@ -0,0 +1,13 @@
+package io.github.dontmashme.tennis_ladder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisLadderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisLadderApplication.class, args);
+	}
+
+}
